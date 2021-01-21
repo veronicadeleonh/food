a0b50434cc56e417c1daa74b7202fc3fd7c951da
Veronica De Leon <v.dleon@gmail.com>
2021-01-21 16:48:06 +0100

    search bar 3

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,7 +7,11 @@ const SearchScreen = () => {
     const [term, setTerm] = useState("")
 
     return <View>
-        <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)}/>
+        <SearchBar 
+            term={term} 
+            onTermChange={newTerm => setTerm(newTerm)}
+            onTermSubmit={() => console.log("term was submitted")}
+            />
         <Text>Search Screen</Text>
     </View>
 }
