cdfee9db838c5577293a8e56649e953a3c70d2a5
Veronica De Leon <v.dleon@gmail.com>
2021-01-25 15:47:59 +0100

    images rendering

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,16 +1,22 @@
 import React from "react"
-import { View, Text, StyleSheet, FlatList } from "react-native"
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native"
+import { withNavigation } from "react-navigation"
 import ResultsDetail from "./ResultsDetail"
 
-const ResultsList = ({ title, results }) => {
+const ResultsList = ({ title, results, navigation }) => {
     return <View style={styles.container}>
         <Text style={styles.title}>{title}</Text>
         <FlatList 
             horizontal
+            showsHorizontalScrollIndicator={false}
             data={results}
             keyExtractor={(result) => result.id}
             renderItem={({ item }) => {
-                return <ResultsDetail result={item} />
+                return (
+                <TouchableOpacity onPress={() => navigation.navigate("ResultsShow", { id: item.id })}>
+                    <ResultsDetail result={item} />
+                </TouchableOpacity>
+                )
             }}
         />
     </View>
@@ -28,4 +34,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default ResultsList
+export default withNavigation(ResultsList)
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, ScrollView } from "react-native"
 import SearchBar from "../components/SearchBar"
 import useResults from "../hooks/useResults"
 import ResultsList from "../components/ResultsList"
@@ -18,18 +18,19 @@ const SearchScreen = () => {
         })
     }
 
-    return <View>
+    return <View style={{ flex: 1 }}>
         <SearchBar 
             term={term} 
             onTermChange={setTerm}
             onTermSubmit={() => searchApi(term)}
             />
         {errorMessage ? <Text>{errorMessage}</Text> : null}
-        <Text>We have found {results.length} results</Text>
 
+        <ScrollView>
         <ResultsList results={filterResultsByPrice("€")} title="Cost Effective"/>
         <ResultsList results={filterResultsByPrice("€€")} title="Bit pricer"/>
         <ResultsList results={filterResultsByPrice("€€€")} title="Big spender"/>
+        </ScrollView>
     </View>
 }
 
